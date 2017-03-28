7f7b051c63537c29119e9e38e24db3de87da28c8
Corneezius <cfr2116@columbia.edu>
2017-03-28 01:31:00 -0400

    add basic pre-loader template

---
+++ js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+		$('#credit').hide(0).fadeIn(1000);
+
+	setTimeout(function(){
+		$('#credit').fadeOut(1000);
+	}, 1500);
+	//
+	setTimeout(function(){
+		$('#title').show();
+	}, 3000);
+	//
+  // setTimeout(function(){
+	// 	$('#title').hide();
+  // }, 6000);
+	// 	//
+  // setTimeout(function(){
+	// 	  $('body').addClass('loaded');
+	// 		$('body').removeClass("noscroll");
+	// 	}, 6400);
+ });
