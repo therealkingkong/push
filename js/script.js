73ec0b15d2136b00cc1799566a67a7fd577da64b
Corneezius <cfr2116@columbia.edu>
2017-08-24 12:19:08 -0400

    media queries for rumble credit

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 $(document).ready(function() {
 		$('#rumble-credit').hide(0).fadeIn(1000);
 
-	setTimeout(function(){
-		$('#rumble-credit').fadeOut(1000);
-	}, 1500);
+		setTimeout(function(){
+			$('#rumble-credit').fadeOut(1000);
+		}, 1500);
 
-	setTimeout(function(){
-		$('#project-title').show();
-		$('#artist-name').show();
-	}, 3000);
+		setTimeout(function(){
+			$('#project-title').show();
+			$('#artist-name').show();
+		}, 3000);
 
-  setTimeout(function(){
-		$('#project-title').hide();
-		$('#artist-name').hide();
-  }, 6000);
+	  // setTimeout(function(){
+		// 	$('#project-title').hide();
+		// 	$('#artist-name').hide();
+	  // }, 6000);
 
 	// 	//
   // setTimeout(function(){
