a196ca6455a0f310839b0fb78ab42f1d79b7aa4f
Cornelius Range V <cfr2116@columbia.edu>
2020-12-30 21:30:58 -0500

    Update secret.js

--- js/secret.js
+++ js/secret.js
@@ -16,6 +16,109 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 var myFirebaseRef = firebase.database().ref();
 var url = window.location.href;
+var unique_id = moment().unix();
+var time = Date();
+
+// get visit data
+jQuery(function($) {
+    $.getJSON("https://ipapi.co/json/", function(response) {
+      console.log(response.country);
+      console.log(response.city);
+      console.log(response.region);
+      console.log(response.timezone);
+      myFirebaseRef.child(unique_id).set({
+        click:false,
+        email: 'null',
+        url: url,
+        country:response.country,
+        city:response.city,
+        region: response.region,
+        timezone:response.timezone,
+        time:time,
+
+      });
+    });
+  });
+
+
+  // get email and handle //
+
+  function handleForm(event) {
+    // prevent page reload
+    event.preventDefault();
+    var userEmail = document.getElementById('email').value;
+
+    signupError.innerHTML = '';
+    signupSuccess.innerHTML = '';
+    if (userEmail.includes("@")) {
+
+    jQuery(function($) {
+        $.getJSON("https://ipapi.co/json/", function(response) {
+          console.log(response.country);
+          console.log(response.city);
+          console.log(response.region);
+          console.log(response.timezone);
+          myFirebaseRef.child(unique_id).set({
+            click:true,
+            email: userEmail,
+            url: url,
+            country:response.country,
+            city:response.city,
+            region: response.region,
+            timezone:response.timezone,
+            time:time,
+
+
+          });
+        });
+      });
+
+
+      jQuery(function($) {
+        $("#signup-success").html("Thanks, we'll be in touch soon!").show().delay(3000).fadeOut(2000);
+        $("#modalLoginForm").modal("hide");
+      });
+    } else {
+      document.getElementById("email").value = "";
+      jQuery(function($) {
+        $("#signup-error").html('Please make sure you entered your e-mail correctly!').show().delay(3000).fadeOut(2000);
+        $("#modalLoginForm").modal("hide");
+      });
+      return false;
+    }
+    document.getElementById("email").value = "";
+  };
+
+
+// get focused push and url //
+  function handlePush(event) {
+    jQuery(function($) {
+        $.getJSON("https://ipapi.co/json/", function(response) {
+          console.log(response.country);
+          console.log(response.city);
+          console.log(response.region);
+          console.log(response.timezone);
+          myFirebaseRef.child(unique_id).set({
+            click:true,
+            email: 'null',
+            url: url,
+            country:response.country,
+            city:response.city,
+            region: response.region,
+            timezone:response.timezone,
+            time:time,
+
+
+          });
+        });
+      });
+
+
+
+  }
+
+
+  // message handle and append
 
 var signupSuccess = document.getElementById('signup-success');
 var signupError = document.getElementById('signup-error');
@@ -23,42 +126,6 @@ var signupError = document.getElementById('signup-error');
 
 var form = document.getElementById("signup-form");
 var pushBtn = document.getElementById("push-btn");
-
-function handleForm(event) {
-  // prevent page reload
-  event.preventDefault();
-  var userEmail = document.getElementById('email').value;
-
-  signupError.innerHTML = '';
-  signupSuccess.innerHTML = '';
-  if (userEmail.includes("@")) {
-    myFirebaseRef.push({
-      email: userEmail,
-      url: url
-    });
-    jQuery(function($) {
-      $("#signup-success").html("Thanks, we'll be in touch soon!").show().delay(3000).fadeOut(2000);
-      $("#modalLoginForm").modal("hide");
-    });
-  } else {
-    document.getElementById("email").value = "";
-    jQuery(function($) {
-      $("#signup-error").html('Please make sure you entered your e-mail correctly!').show().delay(3000).fadeOut(2000);
-      $("#modalLoginForm").modal("hide");
-    });
-    return false;
-  }
-  document.getElementById("email").value = "";
-};
-
-function handlePush(event) {
-
-  myFirebaseRef.push({
-    email: 'null',
-    url: url
-  });
-
-}
 
 form.addEventListener('submit', handleForm);
 pushBtn.addEventListener('click', handlePush);
